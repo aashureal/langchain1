2f444be3d75afde9c588aee7e2d0383dafbe0e83
Aashutosh Uikey <aashureal01@gmail.com>
2025-11-23 10:57:52 +0530

    propmt template

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { PromptTemplate } from "@langchain/core/prompts";
 import { config } from "dotenv";
 config();
+
+
+const promptTemplate = new PromptTemplate({
+  template: `explain {topic} in very simple way like ELI5, make sure to include the core concepts and avoid unnecessary jargons. make the answer as concise as possible.`,
+  inputVariables: ["topic"],
+});
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash",
@@ -15,6 +22,15 @@ const model = new ChatGoogleGenerativeAI({
 //   ],
 // ]);
 
-const res = await model.invoke("who are you?");
+// const res = await model.invoke("who are you?");
 
-console.log(res.content);
+async function main() {
+  try {
+    const res = await promptTemplate.pipe(model).invoke({ topic: "AI" });
+    console.log(res.content);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+main();
